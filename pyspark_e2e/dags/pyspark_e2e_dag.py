b055247c60f2e352a207330aa505521479974921
IlayaBharathi260199 <you@example.com>
2024-11-23 15:34:40 +0530

    e2e added

--- pyspark_e2e/dags/pyspark_e2e_dag.py
+++ pyspark_e2e/dags/pyspark_e2e_dag.py
@@ -8,6 +8,30 @@ from airflow.exceptions import AirflowException
 
 
 
+
+def on_failure_callback(context):
+    # Import necessary modules
+    from airflow.utils.email import send_email
+    
+    # Extract relevant information from the context
+    task_instance = context['task_instance']
+    exception = context['exception']
+    dag_run = context['dag_run']
+    
+    # Prepare email content
+    subject = f"Airflow Failure: Task {task_instance.task_id} in DAG {dag_run.dag_id}"
+    html_content = f"""
+    <h3>Task Failure Details</h3>
+    <ul>
+        <li><strong>Task ID:</strong> {task_instance.task_id}</li>
+        <li><strong>DAG ID:</strong> {dag_run.dag_id}</li>
+        <li><strong>Exception:</strong> {exception}</li>
+    </ul>
+    """
+    
+    # Send email
+    send_email('ilayabharathi334@gmail.com', subject, html_content)
+
 # Default arguments for the DAG
 default_args = {
     'owner': 'airflow',
@@ -33,7 +57,7 @@ with DAG(
 spark_job = SparkSubmitOperator(
     task_id='spark_submit_task',
     application='/home/ilaya/pyspark/pyspark_e2e_dag.py',  # Path to your Spark application
-    conn_id='your_spark_connection',  # Connection ID configured in Airflow
+    conn_id='spark_e2e',  # Connection ID configured in Airflow
     conf={
         'spark.master': 'spark://spark-master:7077',
         'spark.executor.memory': '2g',
@@ -41,24 +65,17 @@ spark_job = SparkSubmitOperator(
         'spark.executor.cores': '1',
         'spark.num.executors': '2'
     },
+    on_failure_callback=on_failure_callback,
     dag=dag
 )
-
-    # Email task to send email on success
-send_email_task = EmailOperator(
-        task_id='send_email',
-        to='ilayabharathi334@gmail.com',  # Replace with your email address
-        subject='DAG Success: {{ task_instance.task_id }}',
-        html_content='The print_date task has completed successfully.',
-    )
 
     # End task
 end_task = DummyOperator(
         task_id='end',
     )
 
-    # Set task dependencies: start -> print date -> end
 
-start_task >> spark_job >> send_email_task >> end_task
+start_task >> spark_job >> end_task
+
 
 
